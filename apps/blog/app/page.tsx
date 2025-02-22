fa45c96883677fd335636eaa76bd5c0da0b1fd46
Seowoo Choi <264826@naver.com>
2025-02-22 22:59:59 +0900

    Setting: blog directory

---
+++ apps/blog/app/page.tsx
@@ -0,0 +1,28 @@
+import Image, { type ImageProps } from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+type Props = Omit<ImageProps, "src"> & {
+  srcLight: string;
+  srcDark: string;
+};
+
+const ThemeImage = (props: Props) => {
+  const { srcLight, srcDark, ...rest } = props;
+
+  return (
+    <>
+      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} src={srcDark} className="imgDark" />
+    </>
+  );
+};
+
+export default function Home() {
+  return (
+    <div>
+      <Image src={'/logo_png.png'} alt={""} width={500}
+      height={500} />
+    </div>
+  )
+}
