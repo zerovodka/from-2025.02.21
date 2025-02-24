3d97b116e98abd313e1d9c6ffec184fb351a2137
Seowoo Choi <264826@naver.com>
2025-02-25 01:13:27 +0900

    Test: Three.js

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -5,9 +5,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Zerovodka[blog]",
   description: "Jr. FE Developer Zerovodka's blog",
-  keywords: ["Frontend", "Next.js", "React", "Typescript"],
-  authors: [{ name: "Zerovodka", url: "https://github.com/zerovodka" }],
-  openGraph: { images: "/logo_png.png" },
 };
 
 export default function RootLayout({
