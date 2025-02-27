156ca85471174681eaec9a02e47eab1a3bd3fec0
Seowoo Choi <264826@naver.com>
2025-02-27 18:27:13 +0900

    Test: Vercel build Error

---
+++ apps/blog/next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+
+  //   return config;
+  // },
+
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+};
+
+export default nextConfig;
